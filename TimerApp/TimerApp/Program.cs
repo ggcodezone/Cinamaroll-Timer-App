66551bbe6c0ef9e03d9bb65964098d2dfd1eb7bf
Dank <ggkalasky@gmail.com>
2020-09-10 17:25:02 -0400

    Added Header

--- TimerApp/TimerApp/Program.cs
+++ TimerApp/TimerApp/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//Name: Cinnamaroll Timer App
+//Author: Gwen Kalasky
+//Synopsis: This application can be used as a stopwatch. Click start to begin the timer and stop to pause.
+//Date: 9/2/20
 namespace TimerApp
 {
     static class Program
